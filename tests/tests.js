982114659f25a28aabf0efcd35af8721fc536628
alu0100837353 <alu0100837353@ull.edu.es>
2015-03-25 21:25:24 +0000

    Added test

---
+++ tests/tests.js
@@ -0,0 +1,19 @@
+var assert = chai.assert;
+
+suite('lexical_analyzer', function() {
+    
+    suite('Tokens Flows', function() {
+        test('One Statement, one token', function() {
+            INPUT.value = 'var a = "a";';
+            main();
+            assert.deepEqual(OUTPUT.value, '{"value": "=","arity": "binary","first": { "value": "a", "arity": "name" },"second": {"value": "a","arity": "literal"}}')
+        });
+    });
+    suite('Error', function() {
+        test('Input Error', function() {
+            INPUT.value = 'var a = "a"';
+            main();
+            assert.deepEqual(OUTPUT.value, '{"value": "=","arity": "binary","first": { "value": "a", "arity": "name" },"second": {"value": "a","arity": "literal"}}')
+        });
+    });
+});
